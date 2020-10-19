d0864fa38a11f1ae84163f513ca4c5309c4ef140
sweenutt <sweenutt@192.168.3.19>
2020-10-20 02:23:51 +0700

    Latihan 16 Program Proses Penugasan

---
+++ src/pbo2_if2_10116414_latihan16_penugasan/PBO2_IF2_10116414_Latihan16_Penugasan.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_if2_10116414_latihan16_penugasan;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Penugasan
+ */
+public class PBO2_IF2_10116414_Latihan16_Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c =  a + b;
+        System.out.println("c = a = b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c); 
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+    }
+    
+}
